c59138f452b9dd32a255161ec35dfdd9f09d5c96
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-01-18 14:17:41 -0300

    feat: aula 05 - parte 01
    Iniciar template dos cards da tela inicial.

---
+++ aula-05/PlacesApp/PlacesApp.Mobile/PlacesApp.Mobile/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace PlacesApp.Mobile;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnSleep()
+    {
+    }
+
+    protected override void OnResume()
+    {
+    }
+}
---
+++ aula-05/PlacesApp/PlacesApp.Mobile/PlacesApp.Mobile/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace PlacesApp.Mobile;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
